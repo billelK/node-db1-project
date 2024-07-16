33a58fe7833406ef77c2bc43718974e05a5ba94a
billelK <bilalkhansal@gmail.com>
2024-07-16 16:52:35 +0200

    All Tests Pass

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,11 +2,11 @@ const db = require("../../data/db-config")
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
-  if(!req.body.name || !req.body.budget) {
+  if(req.body.name === undefined || req.body.budget === undefined) {
     res.status(400).json({message: "name and budget are required"})
   } else if (!(req.body.name.trim().length >= 3) || !(req.body.name.trim().length <= 100)) {
     res.status(400).json({message: "name of account must be between 3 and 100"})
-  } else if (typeof req.body.budget !== "number" ) {
+  } else if (typeof req.body.budget !== "number" || isNaN(req.body.budget) ) {
     res.status(400).json({message: "budget of account must be a number"})
   } else if (!(req.body.budget >= 0) || !(req.body.budget <= 1000000)) {
     res.status(400).json({message: "budget of account is too large or too small"})
